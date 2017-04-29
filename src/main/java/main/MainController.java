f8a95356d75ea8b323b29246df0479e8304356fb
Daniel <sflock1@gmail.com>
2017-04-29 18:39:27 +0200

    Fixed intellij error.

--- src/main/java/main/MainController.java
+++ src/main/java/main/MainController.java
@@ -1,26 +1,17 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
+import common.dto.User;
+import common.persistence.CommonPersistence;
+import jpa.repositories.UserRepository;
+import jpa.services.impl.Report;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
-
-import common.dto.User;
-import common.persistence.CommonPersistence;
-import jpa.repositories.UserRepository;
-import jpa.services.impl.Report;
 import participationSystem.hello.dto.Category;
 import participationSystem.hello.dto.Comment;
 import participationSystem.hello.dto.Proposal;
@@ -30,6 +21,10 @@ import participationSystem.hello.model.ControlAdmin;
 import participationSystem.hello.persistence.CommentDao;
 import participationSystem.hello.persistence.ProposalDao;
 import participationSystem.hello.producers.KafkaProducer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 @Controller
 @ComponentScan(basePackages = {"jpa","participationSystem"})
@@ -48,15 +43,15 @@ public class MainController {
 
     private final Report report;
 
-	@Autowired
-	private UserRepository repository;
+	private final UserRepository repository;
 
     @SuppressWarnings("SpringJavaAutowiringInspection")
     @Autowired
-    public MainController(KafkaProducer kafkaProducer, Report report) {
+    public MainController(KafkaProducer kafkaProducer, Report report, UserRepository repository) {
         this.kafkaProducer = kafkaProducer;
         this.report = report;
-    }
+		this.repository = repository;
+	}
 
     @RequestMapping("/dashboard")
 	public String landing() throws InterruptedException {
